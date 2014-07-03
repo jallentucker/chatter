fa63101b4b9a92f6b0fdc5f488eeea4d4d228724
J. Allen Tucker <j.allentucker@gmail.com>
2014-07-03 11:55:15 -0700

    added test fixtures.

--- test/server/test.js
+++ test/server/test.js
@@ -11,16 +11,44 @@ var baseURL = util.format('http://localhost:%d', port);
 
 var expect = require('chai').expect;
 
+var requestFixture = function(fixture) {
+  var requestOptions = {
+    url: baseURL + fixture.request.url,
+    method: fixture.request.method,
+    headers: fixture.request.headers,
+    body: JSON.stringify(fixture.request.json)
+  };
+
+  return requestAsync(requestOptions);
+};
 
 describe('server', function() {
   before(function(done) { this.server = app.listen(port, function() { done(); }); });
   after(function(done) { this.server.close(done); });
 
+  afterEach(function() {
+    // TODO: clear all chirps from the databse
+  });
+
   it('gets no chirps when database is empty', function(done) {
-    requestAsync(baseURL + '/api/chirps').spread(function(response, body) {
+    var fixture = __fixture('chirps-none');
+    requestFixture(fixture).spread(function(response, body) {
       var json = JSON.parse(body);
-      expect(json).to.eql({'chirps':[]});
+      expect(json).to.eql(fixture.response.json);
     })
     .done(function() { done(); }, done);
+  });
+
+  it.skip('gets a list of chirps when database has stuff', function(done) {
+    var fixture = __fixture('chirps-three');
+
+    // TODO: add three chirps to the database
+
+    requestFixture(fixture).spread(function(response, body) {
+      var json = JSON.parse(body);
+      expect(json).to.eql(fixture.response.json);
+    })
+    .done(function() { done(); }, done);
+
   });
 });
